43e0db63beacf44bc68a719a10fa2c840e044161
Michael Belete <50682242+michaelbelete@users.noreply.github.com>
2019-12-29 22:40:01 -0500

    other stuff

--- src/Launcher/Main.java
+++ src/Launcher/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("@../views/login.fxml"));
         Scene scene = new Scene(root);
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(scene);
